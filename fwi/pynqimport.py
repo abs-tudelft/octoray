2e2edb90990bbd9f5c3b20a85bb66ea6bcb12a28
Luc Dierick <ladierick@gmail.com>
2022-07-12 12:03:54 +0200

    clean up cnv

--- fwi/pynqimport.py
+++ fwi/pynqimport.py
@@ -1,8 +1,25 @@
 from pynq import Device, Overlay
 import json
+import socket
+  
 
 dev = Device.devices
 with open("cluster_config.json") as f:
 	config = json.load(f)
 
-ol = Overlay(config["overlay"],download=True,device=dev[0])
+overlay = config["overlay"]
+if isinstance(overlay,list):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.254.254.254', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    overlay = overlay[config["hosts"].index(IP)]
+                      
+        
+ol = Overlay(overlay,download=True,device=dev[0])
